c665ab48ca8a125c22c2f209c791e27b53872f78
Joe Mainwaring <jm@theaccordance.com>
2017-05-02 16:07:50 -0500

    rewrote rest endpoints to directly interact with transmission-rpc

--- apps/transmission.js
+++ apps/transmission.js
@@ -2,98 +2,68 @@ module.exports = function (legos) {
     "use strict";
     var express = require('express'),
         router = express.Router(),
-        Transmission = require('transmission'),
-        transmission = new Transmission({
-            host: legos.transmission.host,
-            port: legos.transmission.port
-        });
+        request = require('request'),
+        sessionId = null;
 
-    // return application status
-    router.get('/', function (req, res) {
-        // var state = {
-        //     session: transmission.session(callback),
-        //     sessionStats: transmission.sessionStats(callback),
-        //     freeSpace: transmission.freeSpace('path', callback)
-        // };
-        // Return status of Transmission
-        // return free space
-        console.log('transmission:state');
-        transmission.session(function (err, session) {
-            if (err) {
-                return console.error(err);
-            }
-
-            transmission.sessionStats(function (err, sessionStats) {
-                if (err) {
-                    return console.error(err);
+    function getTorrent(req, res) {
+        var body = {
+                method: 'torrent-get',
+                arguments: {
+                    fields: ['activityDate', 'addedDate', 'bandwidthPriority', 'comment', 'corruptEver', 'creator', 'dateCreated', 'desiredAvailable', 'doneDate', 'downloadDir', 'downloadedEver', 'downloadLimit', 'downloadLimited', 'error', 'errorString', 'eta', 'files', 'fileStats', 'hashString', 'haveUnchecked', 'haveValid', 'honorsSessionLimits', 'id', 'isFinished', 'isPrivate', 'leftUntilDone', 'magnetLink', 'manualAnnounceTime', 'maxConnectedPeers', 'metadataPercentComplete', 'name', 'peer-limit', 'peers', 'peersConnected', 'peersFrom', 'peersGettingFromUs', 'peersKnown', 'peersSendingToUs', 'percentDone', 'pieces', 'pieceCount', 'pieceSize', 'priorities', 'rateDownload', 'rateUpload', 'recheckProgress', 'seedIdleLimit', 'seedIdleMode', 'seedRatioLimit', 'seedRatioMode', 'sizeWhenDone', 'startDate', 'status', 'trackers', 'trackerStats', 'totalSize', 'torrentFile', 'uploadedEver', 'uploadLimit', 'uploadLimited', 'uploadRatio', 'wanted', 'webseeds', 'webseedsSendingToUs']
                 }
+            },
+            options = {
+                url: 'http://localhost:9091/transmission/rpc',
+                method: 'POST',
+                headers: {
+                    'x-transmission-session-id': sessionId
+                }
+            };
 
-                return res.json({
-                    session: session,
-                    sessionStats: sessionStats
-                });
-            })
-        });
-
-    });
-
-    //updates application
-    router.put('/', function (req, res) {
-        console.log('transmission:update');
-        return res.json({mock: true});
-    });
-
-    // returns list of torrents
-    router.get('/torrents/', function (req, res) {
-        // Return list of torrents
-        console.log('transmission:torrents');
-        transmission.get(function (err, torrents) {
-            if (err) {
-                return console.error(err);
+        function callback(error, response, body) {
+            if (error) {
+                console.log(error);
+                return res.json({error: true});
             }
-            return res.json(torrents);
-        });
 
-    });
+            if (response.statusCode === 409) {
+                console.log('statuscode:409');
+                options.headers['x-transmission-session-id'] = response.headers['x-transmission-session-id'];
+                return request(options, callback);
+            }
 
-    // // returns info on a specific torrent
-    // router.get('/torrents/:id', function (req, res) {
-    //     console.log('torrent:get', req.params.id);
-    //     transmission.get(req.params.id, function (err, torrents) {
-    //         if (err) {
-    //             return res.json(err);
-    //         }
-    //         return res.json(torrents);
-    //     });
-    // });
 
-    // // creates new torrent
-    // router.post('/torrents/', function (req, res) {
-    //     var addOptions = {},
-    //         result = transmission.addUrl(req.params.id, callback);
-    //     // Add Torrent
-    //     console.log('torrent:add');
-    //     return res.json(result);
-    // });
+            if (response) {
+                console.log(response.statusCode);
+            }
+            return res.json(JSON.parse(body));
+        }
 
-    // // updates a specific torrent
-    // router.put('/torrents/:id/start', function (req, res) {
-    //     console.log('start', req.params.id);
-    //     return transmission.start(req.params.id, function (err, torrents) {
-    //         if (err) {
-    //             return res.json(err);
-    //         }
-    //         console.log('start:success', torrents);
-    //         return res.json(torrents);
-    //     });
-    // });
+        if (req.params.id) {
+            body.arguments.ids = [parseInt(req.params.id)];
+        }
 
-    // // remove a torrent;
-    // router.delete('/torrents/:id', function (req, res) {
-    //     // Remove Torrent
-    //     console.log('torrent:remove');
-    //     return res.json(transmission.remove(req.params.id, callback));
-    // });
+        options.body = JSON.stringify(body);
+
+        request(options, callback);
+    }
+
+
+
+
+    // REST endpoints
+
+    // router.route('/')
+    //     .get(getTransmission);
+    //     .put();
+    router.route('/torrents/')
+        .get(getTorrent);
+    //     .post()
+    //     .put();
+    router.route('/torrents/:id')
+        .get(getTorrent);
+    //     .put()
+    //     .delete();
 
     return router;
 };
